190a837c8021508d5a21ab70ac4478f96255cef6
mujahid-ism <mujahidul.islam@selise.ch>
2023-08-23 01:29:15 +0600

    Model & Relationship

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -7,8 +7,16 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ('slug',)
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('author', 'rating')
-    list_display = ('title', 'rating', 'author')
+    list_filter = ('author', 'rating',)
+    list_display = ('title', 'rating', 'author',)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('first_name', 'last_name',)
+    list_display = ('full_name', 'address',)
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
